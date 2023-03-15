3a8b340bbd601aaded2c4b80a9d20dc88c29ee78
Mwni <manuel@tigmar.de>
2023-03-15 22:13:37 +0100

    use x2 realesr variant

--- enhancekit/realesrgan.py
+++ enhancekit/realesrgan.py
@@ -1,23 +1,23 @@
 import torch
 from realesrgan import RealESRGANer
-from realesrgan.archs.srvgg_arch import SRVGGNetCompact
+from basicsr.archs.rrdbnet_arch import RRDBNet
 
 
 class RealESRGan():
-	def __init__(self, checkpoint_path, scale=2, tile=400, tile_pad=10, pre_pad=0, half=None):
+	def __init__(self, checkpoint, scale=2, tile=400, tile_pad=10, pre_pad=0, half=None):
 		if half is None:
 			half = not torch.cuda.is_available()
 
 		self.upsampler = RealESRGANer(
-			scale=scale,
-			model_path=checkpoint_path,
-			model=SRVGGNetCompact(
+			scale=2,
+			model_path=checkpoint,
+			model=RRDBNet(
 				num_in_ch=3, 
 				num_out_ch=3, 
 				num_feat=64, 
-				num_conv=32, 
-				upscale=4, 
-				act_type='prelu'
+				num_block=23, 
+				num_grow_ch=32, 
+				scale=2
 			),
 			tile=tile,
 			tile_pad=tile_pad,
@@ -26,4 +26,4 @@ class RealESRGan():
 		)
 
 	def __call__(self, *args, **kwargs):
-		return self.upsampler.enahnce(*args, **kwargs)
+		return self.upsampler.enhance(*args, **kwargs)
